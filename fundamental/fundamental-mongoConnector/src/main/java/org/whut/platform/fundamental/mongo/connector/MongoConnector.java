c7f885bd48d117b2270c19d525b6c2cdd4ae738c
xiaozhujun <xiaozhujun520@163.com>
2014-03-05 11:28:56 +0800

    创建mongo和redis  connector

---
+++ fundamental/fundamental-mongoConnector/src/main/java/org/whut/platform/fundamental/mongo/connector/MongoConnector.java
@@ -0,0 +1,99 @@
+package org.whut.platform.fundamental.mongo.connector;
+
+import com.mongodb.*;
+import com.mongodb.util.JSON;
+import org.bson.types.ObjectId;
+import org.whut.platform.fundamental.config.FundamentalConfigProvider;
+import org.whut.platform.fundamental.logger.PlatformLogger;
+
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: xiaozhujun
+ * Date: 13-11-1
+ * Time: 下午5:54
+ * To change this template use File | Settings | File Templates.
+ */
+public class MongoConnector {
+
+    private static PlatformLogger logger = PlatformLogger.getLogger(MongoConnector.class);
+
+    private static Mongo mongo;
+    private String dbName;
+    private String collectionName;
+
+    static {
+        String host = FundamentalConfigProvider.get("mongo.host");
+        String port = FundamentalConfigProvider.get("mongo.port");
+        try {
+            mongo = new Mongo(host,Integer.parseInt(port));
+            logger.info("mongo is initialized,host is "+host+" ,port is "+port);
+        } catch (UnknownHostException e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+    }
+
+    public static Mongo getMongo(){
+         return mongo;
+    }
+
+    public static DB getDB(String dbName){
+        return mongo.getDB(dbName);
+    }
+
+
+
+    //传感器数据舒服构造函数
+    public MongoConnector(String dbName, String collectionName){
+        this.dbName = dbName;
+        this.collectionName = collectionName;
+        mongo = MongoConnector.getMongo();
+    }
+
+    //保存文档对象
+    public String insertDocument(String documentJSON){
+        DB db = mongo.getDB(dbName);
+        DBCollection collection = db.getCollection(collectionName);
+        DBObject document = (DBObject) JSON.parse(documentJSON);
+        WriteResult result = collection.insert(document);
+        Object objectId = document.get("_id");
+        if(objectId!=null){
+            return ((ObjectId)objectId).toStringMongod();
+        }
+        return null;
+    }
+
+    public String insertDocumentObject(DBObject documentObject){
+        DB db = mongo.getDB(dbName);
+        DBCollection collection = db.getCollection(collectionName);
+        WriteResult result = collection.insert(documentObject);
+        Object objectId = documentObject.get("_id");
+        if(objectId!=null){
+            return ((ObjectId)objectId).toStringMongod();
+        }
+        return null;
+    }
+
+    //获取文档对象
+    public DBObject getDocument(String objectID){
+        if(objectID==null){
+            throw new IllegalArgumentException("object id is null");
+        }
+        DB db = mongo.getDB(dbName);
+        DBCollection collection = db.getCollection(collectionName);
+        BasicDBObject query = new BasicDBObject();
+        query.put("_id", new ObjectId(objectID));
+        DBObject document = collection.findOne(query);
+        return document;
+    }
+
+    //获取文档内的数据数组
+    public ArrayList getDocumentData(String objectID){
+        DBObject dbObject = getDocument(objectID);
+        ArrayList data = (ArrayList)dbObject.get("data");
+        return data;
+    }
+}
---
+++ fundamental/fundamental-redisConnector/src/main/java/org/whut/platform/fundamental/redis/connector/RedisConnector.java
@@ -0,0 +1,68 @@
+package org.whut.platform.fundamental.redis.connector;
+
+
+import org.whut.platform.fundamental.config.FundamentalConfigProvider;
+import org.whut.platform.fundamental.logger.PlatformLogger;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+
+import java.util.ResourceBundle;
+
+public class RedisConnector {
+
+    private static PlatformLogger logger = PlatformLogger.getLogger(RedisConnector.class);
+
+    private static int expire;
+    private static JedisPool pool;
+    static {
+        ResourceBundle bundle = ResourceBundle.getBundle("redis");
+        if (bundle == null) {
+            throw new IllegalArgumentException("[redis.properties] is not found!");
+        }
+        JedisPoolConfig config = new JedisPoolConfig();
+        config.setMaxActive(Integer.valueOf(FundamentalConfigProvider.get("redis.pool.maxActive")));
+        config.setMaxIdle(Integer.valueOf(FundamentalConfigProvider.get("redis.pool.maxIdle")));
+        config.setMaxWait(Long.valueOf(FundamentalConfigProvider.get("redis.pool.maxWait")));
+        config.setTestOnBorrow(Boolean.valueOf(FundamentalConfigProvider.get("redis.pool.testOnBorrow")));
+        config.setTestOnReturn(Boolean.valueOf(FundamentalConfigProvider.get("redis.pool.testOnReturn")));
+        pool = new JedisPool(config, FundamentalConfigProvider.get("redis.ip"), Integer.valueOf(FundamentalConfigProvider.get("redis.port")));
+        logger.info("redis.ip: "+FundamentalConfigProvider.get("redis.ip"));
+        expire = Integer.valueOf(FundamentalConfigProvider.get("redis.expire"));
+    }
+
+    public RedisConnector() {}
+
+    public int getExpire() {
+        return expire;
+    }
+
+    public boolean set(String key,String value){
+        Jedis jedis = pool.getResource();
+        try{
+            String result = jedis.set(key,value);
+            if (result.equals("OK")){
+                return true;
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            pool.returnResource(jedis);
+        }
+        return false;
+    }
+
+    public String get(String key){
+        Jedis jedis = pool.getResource();
+        String result = null;
+        try{
+            result = jedis.get(key);
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            pool.returnResource(jedis);
+        }
+        return result;
+    }
+
+}

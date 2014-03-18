156f9f0d16d35aee7f6f492f876935824f772825
xiaozhujun <xiaozhujun520@163.com>
2014-03-18 18:01:00 +0800

    用户权限相关数据库脚本

---
+++ business/business-user/src/main/data/user_authority_power.sql
@@ -0,0 +1,108 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : localhost
+Source Server Version : 50533
+Source Host           : localhost:3306
+Source Database       : riskmanagement
+
+Target Server Type    : MYSQL
+Target Server Version : 50533
+File Encoding         : 65001
+
+Date: 2014-03-18 17:59:22
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for authority
+-- ----------------------------
+DROP TABLE IF EXISTS `authority`;
+CREATE TABLE `authority` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `status` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of authority
+-- ----------------------------
+INSERT INTO `authority` VALUES ('1', 'ROLE_USER', '用户', '1');
+INSERT INTO `authority` VALUES ('2', 'ROLE_ADMIN', '管理员', '1');
+
+-- ----------------------------
+-- Table structure for authority_power
+-- ----------------------------
+DROP TABLE IF EXISTS `authority_power`;
+CREATE TABLE `authority_power` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `authorityId` bigint(20) DEFAULT NULL,
+  `powerId` bigint(20) DEFAULT NULL,
+  `powerResource` varchar(255) DEFAULT NULL,
+  `authorityName` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of authority_power
+-- ----------------------------
+INSERT INTO `authority_power` VALUES ('1', '1', '1', '/rs/**', 'ROLE_USER');
+
+-- ----------------------------
+-- Table structure for power
+-- ----------------------------
+DROP TABLE IF EXISTS `power`;
+CREATE TABLE `power` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `resource` varchar(255) DEFAULT NULL,
+  `type` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of power
+-- ----------------------------
+INSERT INTO `power` VALUES ('1', '/rs/**', 'url');
+INSERT INTO `power` VALUES ('2', '/user.html', 'url');
+INSERT INTO `power` VALUES ('3', '/admin.html', 'url');
+
+-- ----------------------------
+-- Table structure for user
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `sex` varchar(255) DEFAULT NULL,
+  `role` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of user
+-- ----------------------------
+INSERT INTO `user` VALUES ('1', 'xiaozhujun', 'e10adc3949ba59abbe56e057f20f883e', '男', 'admin');
+INSERT INTO `user` VALUES ('2', 'zhangsan', 'hello', 'nan', 'user');
+
+-- ----------------------------
+-- Table structure for user_authority
+-- ----------------------------
+DROP TABLE IF EXISTS `user_authority`;
+CREATE TABLE `user_authority` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `userId` bigint(20) DEFAULT NULL,
+  `authorityId` bigint(20) DEFAULT NULL,
+  `userName` varchar(255) DEFAULT NULL,
+  `authorityName` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of user_authority
+-- ----------------------------
+INSERT INTO `user_authority` VALUES ('1', '1', '1', 'xiaozhujun', 'ROLE_USER');
+INSERT INTO `user_authority` VALUES ('2', '1', '2', 'xiaozhujun', 'ROLE_ADMIN');

a48644be6d0ee72ae6ba955e33ea8d1cb55f38f0
xiaozhujun <xiaozhujun520@163.com>
2014-04-13 22:04:09 +0800

    tab页切换js上传

---
+++ riskManagement/src/main/webapp/js/rightTab.js
@@ -0,0 +1,37 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: ThinkPad
+ * Date: 14-4-13
+ * Time: 下午8:04
+ * To change this template use File | Settings | File Templates.
+ */
+$.extend({
+    switchTab: function switchTab(rankTab,InfoTab,rightRank,rightShow){
+        var rTab="#"+rankTab;
+        var iTab="#"+InfoTab;
+        var rRank="#"+rightRank;
+        var rShow="#"+rightShow;
+        $(rTab).css("background-color","#999999");
+        $(rTab).css("color","#ffffff");
+        $(iTab).click(function(){
+            $(rTab).css("background-color","#F7F7F7");
+            $(iTab).css("background-color","#999999");
+            $(iTab).css("color","#ffffff");
+            $(rTab).css("color","#999999");
+            //$("#rightshow").css("display","block");
+            $(rRank).animate({left:"400px"},rightCallback);
+            $(rShow).animate({left:""});
+            function  rightCallback(){
+                $(rShow).css("display","block");}
+        });
+        $(rTab).click(function(){
+            $(rTab).css("background-color","#999999");
+            $(rTab).css("color","#ffffff");
+            $(iTab).css("color","#999999");
+            $(iTab).css("background-color","#F7F7F7");
+            $(rRank).css("display","block");
+            $(rRank).animate({left:""});
+            $(rShow).animate({left:"400px"});
+        });
+    }
+});

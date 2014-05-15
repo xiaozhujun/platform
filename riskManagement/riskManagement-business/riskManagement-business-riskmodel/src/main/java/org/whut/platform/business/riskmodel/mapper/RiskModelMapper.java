0991448097f09c4fcd32a224bb0b178e61235291
xiaozhujun <xiaozhujun520@163.com>
2014-05-15 14:03:14 +0800

    添加风险计算模块

---
+++ riskManagement/riskManagement-business/riskManagement-business-riskmodel/src/main/java/org/whut/platform/business/riskmodel/mapper/RiskModelMapper.java
@@ -0,0 +1,11 @@
+package org.whut.platform.business.riskmodel.mapper;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ThinkPad
+ * Date: 14-5-15
+ * Time: 下午12:50
+ * To change this template use File | Settings | File Templates.
+ */
+public class RiskModelMapper {
+}

554c6088fbd529d62fbe132f66dfcbb172413a34
xiaozhujun <xiao520@163.com>
2014-05-15 10:30:46 +0800

    点检系统部门上传

---
+++ inspectManagement/inspectManagement-business/inspectManagement-business-Department/src/main/java/org/whut/inspectManagement/business/Department/mapper/DepartmentMapper.java
@@ -0,0 +1,18 @@
+package org.whut.inspectManagement.business.Department.mapper;
+
+import org.whut.inspectManagement.business.Department.entity.Department;
+import org.whut.platform.fundamental.orm.mapper.AbstractMapper;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: chenqw
+ * Date: 14-5-14
+ * Time: 上午11:00
+ * To change this template use File | Settings | File Templates.
+ */
+public interface DepartmentMapper extends AbstractMapper<Department> {
+    public List<Department> findByCondition(Map<String,Object> map);
+}

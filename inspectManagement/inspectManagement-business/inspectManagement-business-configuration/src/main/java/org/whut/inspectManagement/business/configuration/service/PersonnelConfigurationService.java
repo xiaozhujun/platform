db38699244b7fe79c233bb4e756894950069556a
xiaozhujun <xiao520@163.com>
2014-05-18 12:31:21 +0800

    配置管理

---
+++ inspectManagement/inspectManagement-business/inspectManagement-business-configuration/src/main/java/org/whut/inspectManagement/business/configuration/service/PersonnelConfigurationService.java
@@ -0,0 +1,11 @@
+package org.whut.inspectManagement.business.configuration.service;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: John
+ * Date: 14-5-18
+ * Time: 下午12:08
+ * To change this template use File | Settings | File Templates.
+ */
+public class PersonnelConfigurationService {
+}

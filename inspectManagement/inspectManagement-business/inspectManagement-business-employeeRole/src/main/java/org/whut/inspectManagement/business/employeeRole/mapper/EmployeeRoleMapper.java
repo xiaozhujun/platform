8bc41fcf167baa9b341d3c0811cc5a32994f329e
xiaozhujun <xiao520@163.com>
2014-05-19 20:32:07 +0800

    员工角色添加功能完善

--- inspectManagement/inspectManagement-business/inspectManagement-business-employeeRole/src/main/java/org/whut/inspectManagement/business/employeeRole/mapper/EmployeeRoleMapper.java
+++ inspectManagement/inspectManagement-business/inspectManagement-business-employeeRole/src/main/java/org/whut/inspectManagement/business/employeeRole/mapper/EmployeeRoleMapper.java
@@ -1,5 +1,6 @@
 package org.whut.inspectManagement.business.employeeRole.mapper;
 
+import org.apache.ibatis.annotations.Param;
 import org.whut.inspectManagement.business.employeeRole.entity.EmployeeRole;
 import org.whut.platform.fundamental.orm.mapper.AbstractMapper;
 
@@ -15,4 +16,6 @@ import java.util.Map;
  */
 public interface EmployeeRoleMapper extends AbstractMapper{
     public List<EmployeeRole> findByCondition(Map<String,Object> map);
+
+    public long getIdByName(@Param("name") String name,@Param("appId") long appId);
 }

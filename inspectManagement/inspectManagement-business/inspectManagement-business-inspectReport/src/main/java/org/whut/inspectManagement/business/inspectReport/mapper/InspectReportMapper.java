6008d484380af97694765ccf2bc5beec59783f11
xiaozhujun <xiaozhujun520@163.com>
2014-06-10 20:45:34 +0800

    报表查询界面的完成

---
+++ inspectManagement/inspectManagement-business/inspectManagement-business-inspectReport/src/main/java/org/whut/inspectManagement/business/inspectReport/mapper/InspectReportMapper.java
@@ -0,0 +1,15 @@
+package org.whut.inspectManagement.business.inspectReport.mapper;
+
+import org.whut.inspectManagement.business.inspectReport.entity.InspectReport;
+import org.whut.platform.fundamental.orm.mapper.AbstractMapper;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ThinkPad
+ * Date: 14-6-10
+ * Time: 上午9:22
+ * To change this template use File | Settings | File Templates.
+ */
+public interface InspectReportMapper extends AbstractMapper<InspectReport>{
+
+}
--- inspectManagement/inspectManagement-business/inspectManagement-business-inspectReport/src/main/java/org/whut/inspectManagement/business/inspectReport/web/inspectReportServiceWeb.java
+++ inspectManagement/inspectManagement-business/inspectManagement-business-inspectReport/src/main/java/org/whut/inspectManagement/business/inspectReport/web/inspectReportServiceWeb.java
@@ -1,6 +1,23 @@
 package org.whut.inspectManagement.business.inspectReport.web;
 
 
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.stereotype.Component;
+import org.whut.inspectManagement.business.inspectReport.mapper.ReportSqlMapper;
+import org.whut.platform.fundamental.report.PlatformReport;
+import org.whut.platform.fundamental.util.json.JsonResultUtils;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.*;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import java.sql.Connection;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created with IntelliJ IDEA.
  * User: John
@@ -9,6 +26,135 @@ package org.whut.inspectManagement.business.inspectReport.web;
  * To change this template use File | Settings | File Templates.
  */
 
+@Component
+@Path("/inspectReport")
 public class inspectReportServiceWeb {
+    private ReportSqlMapper reportSqlMapper=new ReportSqlMapper();
+    @Autowired
+    private SqlSessionFactory sqlSessionFactory;
+    @Context
+    HttpServletRequest request;
+    @Context
+    HttpServletResponse response;
 
+    private PlatformReport platformReport=new PlatformReport();
+
+    @Produces(MediaType.APPLICATION_JSON+";charset=UTF-8")
+    @POST
+    @Path("/reportSearch")
+    public String reportSearch(@FormParam("sTime")String sTime,@FormParam("eTime")String eTime,@FormParam("deviceId")String deviceId,@FormParam("userId")String userId,@FormParam("flag") String flag){
+        Connection connection=null;
+        try {
+            connection=sqlSessionFactory.getConfiguration().getEnvironment().getDataSource().getConnection();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        if(flag.equals("0")){
+            deviceCount(sTime,eTime,connection);
+        }else if(flag.equals("1")){
+            //deviceInfo
+            deviceInfo(sTime,eTime,deviceId,connection);
+        }else if(flag.equals("2")){
+            //peopleCount
+            peopleCount(sTime,eTime,deviceId,connection);
+        }else if(flag.equals("3")){
+            //peopleInfo
+            peopleInfo(sTime,eTime,deviceId,userId,connection);
+        }else if(flag.equals("4")){
+            //deviceHistory
+            deviceHistory(sTime,eTime,deviceId,connection);
+        }
+        return JsonResultUtils.getCodeAndMesByStringAsDefault(JsonResultUtils.Code.SUCCESS);
+       }
+
+    public String test(String sTime,String eTime,String deviceId,String userId){
+        String sql=null;
+        try{
+            String name="test";
+            Map<String,Object> map=new HashMap<String, Object>();
+            map.put("sTime",sTime);
+            map.put("eTime",eTime);
+            map.put("deviceId",deviceId);
+            map.put("userId",userId);
+            sql=reportSqlMapper.getSql("deviceCountSql",sqlSessionFactory,map);
+            System.out.println(sql + "sql语句");
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return sql;
+    }
+    public void deviceCount(String sTime,String eTime,Connection connection){
+        try{
+            Map<String,Object> map=new HashMap<String, Object>();
+            map.put("sTime",sTime);
+            map.put("eTime",eTime);
+            String deviceSql=reportSqlMapper.getSql("deviceCountSql",sqlSessionFactory,map);
+            Map<String,String> parameters=new HashMap<String, String>();
+            parameters.put("sql",deviceSql);
+            String reportTemplate=request.getSession().getServletContext().getRealPath("/inspectReportTemplate/deviceCount1.jasper");
+            platformReport.getMapToExportReport(reportTemplate,parameters,"html",request,response,"deviceCount",connection);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+    public void deviceInfo(String sTime,String eTime,String deviceId,Connection connection){
+        try{
+            Map<String,Object> map=new HashMap<String, Object>();
+            map.put("sTime",sTime);
+            map.put("eTime",eTime);
+            map.put("deviceId",deviceId);
+            String deviceSql=reportSqlMapper.getSql("deviceInfoSql",sqlSessionFactory,map);
+            Map<String,String> parameters=new HashMap<String, String>();
+            parameters.put("sql",deviceSql);
+            String reportTemplate=request.getSession().getServletContext().getRealPath("/inspectReportTemplate/deviceCount.jasper");
+            platformReport.getMapToExportReport(reportTemplate,parameters,"html",request,response,"deviceInfo",connection);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+    public void peopleCount(String sTime,String eTime,String deviceId,Connection connection){
+        try{
+            Map<String,Object> map=new HashMap<String, Object>();
+            map.put("sTime",sTime);
+            map.put("eTime",eTime);
+            map.put("deviceId",deviceId);
+            String deviceSql=reportSqlMapper.getSql("peopleCountSql",sqlSessionFactory,map);
+            Map<String,String> parameters=new HashMap<String, String>();
+            parameters.put("sql",deviceSql);
+            String reportTemplate=request.getSession().getServletContext().getRealPath("/inspectReportTemplate/peopleCount.jasper");
+            platformReport.getMapToExportReport(reportTemplate,parameters,"html",request,response,"peopleCount",connection);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+    public void peopleInfo(String sTime,String eTime,String deviceId,String userId,Connection connection){
+        try{
+            Map<String,Object> map=new HashMap<String, Object>();
+            map.put("sTime",sTime);
+            map.put("eTime",eTime);
+            map.put("deviceId",deviceId);
+            String peopleInfoSql=reportSqlMapper.getSql("peopleInfoSql",sqlSessionFactory,map);
+            Map<String,String> parameters=new HashMap<String, String>();
+            parameters.put("sql",peopleInfoSql);
+            String reportTemplate=request.getSession().getServletContext().getRealPath("/inspectReportTemplate/reportx2.jasper");
+            platformReport.getMapToExportReport(reportTemplate,parameters,"html",request,response,"peopleInfo",connection);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+    public void deviceHistory(String sTime,String eTime,String deviceId,Connection connection){
+        try{
+            Map<String,Object> map=new HashMap<String, Object>();
+            map.put("sTime",sTime);
+            map.put("eTime",eTime);
+            map.put("deviceId",deviceId);
+            String deviceHistorySql=reportSqlMapper.getSql("deviceHistory",sqlSessionFactory,map);
+            Map<String,String> parameters=new HashMap<String, String>();
+            parameters.put("sql",deviceHistorySql);
+            String reportTemplate=request.getSession().getServletContext().getRealPath("/inspectReportTemplate/deviceHistory1.jasper");
+            platformReport.getMapToExportReport(reportTemplate,parameters,"html",request,response,"deviceHistory",connection);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
 }

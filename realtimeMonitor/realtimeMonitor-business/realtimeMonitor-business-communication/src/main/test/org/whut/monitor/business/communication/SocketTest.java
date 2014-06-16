6135b07d631f53daadf54c2bb1fc20358e426ecc
xiaozhujun <xiao520@163.com>
2014-06-16 21:51:04 +0800

    realtimeMoniltor测试

---
+++ realtimeMonitor/realtimeMonitor-business/realtimeMonitor-business-communication/src/main/test/org/whut/monitor/business/communication/SocketTest.java
@@ -0,0 +1,61 @@
+package org.whut.monitor.business.communication;
+
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.net.Socket;
+import java.util.Date;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: xiaozhujun
+ * Date: 14-5-7
+ * Time: 下午3:55
+ * To change this template use File | Settings | File Templates.
+ */
+public class SocketTest {
+    public static void main(String[] args)  {
+        //为了简单起见，所有的异常都直接往外抛
+        String host = "127.0.0.1";  //要连接的服务端IP地址
+        //String host = "www.cseicms.com";  //要连接的服务端IP地址
+        int port = 38888;   //要连接的服务端对应的监听端口
+        //与服务端建立连接
+        Socket client = null;
+        try {
+            client = new Socket(host, port);
+            Writer writer = new OutputStreamWriter(client.getOutputStream());
+
+            //向服务器端第二次发送字符串
+            long count = 1000000;
+            StringBuffer data = new StringBuffer("");
+            Date startTime = new Date();
+            for (long j=0;j<count;j++){
+                for(int i=0;i<60;i++){
+                    if(i==0){
+                        data.append(Math.round(Math.random()*1500));
+                    }else{
+                        data.append(",").append(Math.round(Math.random()*1500));
+                    }
+
+                }
+                Date now = new Date();
+                String json = "{sensors:[{sensorNum:'301',dataType:'Route',time:"+now.getTime()+",data:["+data+"]}]}";
+                writer.write(json);
+                writer.flush();//写完后要记得flush
+                System.out.println(json);
+                data.delete(0,data.length());
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+                }
+            }
+
+            writer.flush();//写完后要记得flush
+            writer.close();
+            client.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}

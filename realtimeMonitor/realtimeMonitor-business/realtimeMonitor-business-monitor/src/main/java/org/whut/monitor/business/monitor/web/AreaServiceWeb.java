f581e9f86e7734ccfe87ceca591845e15c6f62aa
xiaozhujun <xiao520@163.com>
2014-07-18 00:15:56 +0800

    area管理

--- realtimeMonitor/realtimeMonitor-business/realtimeMonitor-business-monitor/src/main/java/org/whut/monitor/business/monitor/web/AreaServiceWeb.java
+++ realtimeMonitor/realtimeMonitor-business/realtimeMonitor-business-monitor/src/main/java/org/whut/monitor/business/monitor/web/AreaServiceWeb.java
@@ -7,7 +7,6 @@ import org.whut.monitor.business.monitor.entity.Area;
 import org.whut.monitor.business.monitor.entity.SubArea;
 import org.whut.monitor.business.monitor.service.AreaService;
 import org.whut.monitor.business.monitor.service.GroupService;
-import org.whut.platform.business.user.security.UserContext;
 import org.whut.platform.fundamental.util.json.JsonMapper;
 import org.whut.platform.fundamental.util.json.JsonResultUtils;
 
@@ -48,61 +47,68 @@ public class AreaServiceWeb {
     @Produces(MediaType.APPLICATION_JSON +";charset=UTF-8")
     @Path("/add")
     @POST
-//    public String add(@FormParam("jsonString")String jsonString,@FormParam("name")String name,@FormParam("description")String description) {
-//        Group group = JsonMapper.buildNonDefaultMapper().fromJson(jsonString,Group.class);
-//        String groupName = group.getName();
-//        long groupAppId = group.getAppId();
-//        long appId = UserContext.currentUserAppId();
-//        long groupId = groupService.getIdByNameAndAppId(groupName,groupAppId);
-//        Date date = new Date();
-//        if (name.trim().equals("")) {
-//            return JsonResultUtils.getCodeAndMesByString(JsonResultUtils.Code.ERROR.getCode(),"参数不能为空");
-//        }
-//
-//        long id;
-//        try {
-//            id = areaService.getIDByNameAndAppId(name,appId);
-//        } catch (Exception e) {
-//            id = 0;
-//        }
-//
-//        if (id != 0) {
-//            return JsonResultUtils.getCodeAndMesByString(JsonResultUtils.Code.ERROR.getCode(),"区域已存在");
-//        }
-//
-//        Area area = new Area();
-//        area.setAppId(appId);
-//        area.setCreatetime(date);
-//        area.setDescription(description);
-//        area.setGroupId(groupId);
-//        areaService.add(area);
-//        return JsonResultUtils.getCodeAndMesByStringAsDefault(JsonResultUtils.Code.SUCCESS);
-//    }
     public String add(@FormParam("jsonStringList") String jsonStringList) {
-        long appId=UserContext.currentUserAppId();
-        List<Area> areaList = new ArrayList<Area>();
-        List<SubArea> subAreaList = new ArrayList<SubArea>();
+//        long appId=UserContext.currentUserAppId();
+        long appId=1;
         List<SubArea> areaSuccessList = new ArrayList<SubArea>();
         List<SubArea> areaErrorList = new ArrayList<SubArea>();
+        List<SubArea> areaRepeatList=new ArrayList<SubArea>();
         Date date=new Date();
         try {
-            JSONArray jsonArray=new JSONArray(jsonStringList);
+            JSONArray jsonArray = new JSONArray(jsonStringList);
             if(jsonArray.length()==0){
                 return JsonResultUtils.getCodeAndMesByString(JsonResultUtils.Code.ERROR.getCode(),"参数不能为空");
             }
             for(int i=0;i<jsonArray.length();i++){
                 String jsonString= jsonArray.get(i).toString();
-                SubArea subarea=JsonMapper.buildNonDefaultMapper().fromJson(jsonString,SubArea.class);
-                Area area = new Area();
-                String name = subarea.getName();
-                String description = subarea.getDescription();
-                String groupName = subarea.getGroupName();
-                long key = subarea.getId();
+                SubArea subArea=JsonMapper.buildNonDefaultMapper().fromJson(jsonString,SubArea.class);
+                if(subArea.getAddStatus().equals("提交成功")){
+                    areaSuccessList.add(subArea);
                 }
-            }catch (Exception e) {
-
+                else {
+                    if (subArea.getName() == "" || subArea.getGroupName() == "") {
+                        subArea.setAddStatus("参数缺省");
+                        areaErrorList.add(subArea);
+                    }
+                    else {
+                        long tempId;
+                        try {
+                            tempId = areaService.getIDByNameAndAppId(subArea.getName(), appId);
+                        }catch (Exception e) {
+                            tempId = 0;
+                        }
+                        if(tempId != 0){
+                            subArea.setAddStatus("参数重复");
+                            areaRepeatList.add(subArea);
+                        }
+                        else {
+                            Area area = new Area();
+                            area.setName(subArea.getName());
+                            area.setDescription(subArea.getDescription());
+                            area.setGroupId(groupService.getIdByNameAndAppId(subArea.getGroupName(),appId));
+                            area.setCreatetime(date);
+                            area.setAppId(appId);
+//                            area.setId(tempId);
+                            areaService.add(area);
+                            subArea.setAddStatus("提交成功");
+                            areaSuccessList.add(subArea);
+                        }
+                    }
+                }
+            }
+        }catch (Exception e) {
+            e.printStackTrace();
         }
-        return null;
+        if (areaErrorList.size()!=0){
+            areaErrorList.addAll(areaRepeatList);
+            areaErrorList.addAll(areaSuccessList);
+            return JsonResultUtils.getObjectResultByStringAsDefault(areaErrorList,JsonResultUtils.Code.ERROR);
+        }else if(areaRepeatList.size()!=0){
+            areaRepeatList.addAll(areaSuccessList);
+            return JsonResultUtils.getObjectResultByStringAsDefault(areaRepeatList,JsonResultUtils.Code.DUPLICATE);
+        }else {
+            return JsonResultUtils.getObjectResultByStringAsDefault(areaSuccessList,JsonResultUtils.Code.SUCCESS);
+        }
     }
 
     @Produces(MediaType.APPLICATION_JSON +";charset=UTF-8")

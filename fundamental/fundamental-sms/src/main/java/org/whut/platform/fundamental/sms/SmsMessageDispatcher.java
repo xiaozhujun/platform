869067c0cc59391cb611cd682e8ed36fa53efb9a
xiaozhujun <xiaozhujun520@163.com>
2014-08-22 00:34:12 +0800

    短信模块

---
+++ fundamental/fundamental-sms/src/main/java/org/whut/platform/fundamental/sms/SmsMessageDispatcher.java
@@ -0,0 +1,41 @@
+package org.whut.platform.fundamental.sms;
+
+import org.apache.activemq.command.ActiveMQMessage;
+import org.apache.activemq.command.ActiveMQTextMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.whut.platform.fundamental.communication.api.MessageDispatcher;
+import org.whut.platform.fundamental.logger.PlatformLogger;
+import org.whut.platform.fundamental.message.api.PlatformMessageProducer;
+
+import javax.jms.MessageNotReadableException;
+import javax.jms.MessageNotWriteableException;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ThinkPad
+ * Date: 14-8-22
+ * Time: 上午12:14
+ * To change this template use File | Settings | File Templates.
+ */
+public class SmsMessageDispatcher implements MessageDispatcher{
+
+    public static final PlatformLogger logger=PlatformLogger.getLogger(SmsMessageDispatcher.class);
+
+    public static final String destination=Constants.SMS_QUEUE_DESTINATION;
+
+    @Autowired
+    private PlatformMessageProducer platformMessageProducer;
+    @Override
+    public void dispatchMessage(String messageBody) {
+       if(messageBody!=null){
+           logger.info("dispatch:"+messageBody);
+           try{
+               ActiveMQTextMessage message=new ActiveMQTextMessage();
+               message.setText(messageBody);
+               platformMessageProducer.sendTopic(destination,message);
+           }catch(MessageNotWriteableException e){
+               e.printStackTrace();
+           }
+       }
+    }
+}

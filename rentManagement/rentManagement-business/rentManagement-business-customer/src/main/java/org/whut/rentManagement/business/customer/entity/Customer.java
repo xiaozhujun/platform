aa1848c01b9fffc01d640f5849b3f673f936f54b
xiaoshou <xiao520@163.com>
2014-10-18 19:17:32 +0800

    客户管理

---
+++ rentManagement/rentManagement-business/rentManagement-business-customer/src/main/java/org/whut/rentManagement/business/customer/entity/Customer.java
@@ -0,0 +1,121 @@
+package org.whut.rentManagement.business.customer.entity;
+
+import java.util.Date;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: xiaoshou
+ * Date: 14-10-10
+ * Time: 下午3:52
+ * To change this template use File | Settings | File Templates.
+ */
+public class Customer {
+    private long id;
+    private String name;
+    private String description;
+    private String address;
+    private String linkman;
+    private String telephone;
+    private String email;
+    private String qq;
+    private String bank;
+    private String bankAccount;
+    private Date createTime;
+    private long appId;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getLinkman() {
+        return linkman;
+    }
+
+    public void setLinkman(String linkman) {
+        this.linkman = linkman;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getQq() {
+        return qq;
+    }
+
+    public void setQq(String qq) {
+        this.qq = qq;
+    }
+
+    public String getBank() {
+        return bank;
+    }
+
+    public void setBank(String bank) {
+        this.bank = bank;
+    }
+
+    public String getBankAccount() {
+        return bankAccount;
+    }
+
+    public void setBankAccount(String bankAccount) {
+        this.bankAccount = bankAccount;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public long getAppId() {
+        return appId;
+    }
+
+    public void setAppId(long appId) {
+        this.appId = appId;
+    }
+}
---
+++ rentManagement/rentManagement-business/rentManagement-business-customer/src/main/java/org/whut/rentManagement/business/customer/mapper/CustomerMapper.java
@@ -0,0 +1,23 @@
+package org.whut.rentManagement.business.customer.mapper;
+
+import org.apache.ibatis.annotations.Param;
+import org.whut.platform.fundamental.orm.mapper.AbstractMapper;
+import org.whut.rentManagement.business.customer.entity.Customer;
+
+import java.util.List;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: xiaoshou
+ * Date: 14-10-10
+ * Time: 下午5:07
+ * To change this template use File | Settings | File Templates.
+ */
+public interface CustomerMapper extends AbstractMapper<Customer> {
+    public long getIdByName(@Param("name") String name, @Param("appId") long appId);
+    public long getIdByTelephone(@Param("telephone") String telephone);
+    public List<Customer> findByCondition(@Param("appId") long appId, @Param("name") String name, @Param("description") String description,
+                                          @Param("address") String address, @Param("linkman") String linkman, @Param("telephone") String telephone,
+                                          @Param("email") String email, @Param("qq") String qq, @Param("bank") String bank,
+                                          @Param("bankAccount") String bankAccount, @Param("createTime") String createTime);
+}

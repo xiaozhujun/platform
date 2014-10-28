325d24f92595a8f64b91793b847d8d6b47ede210
xiaozhujun <xiao520@163.com>
2014-10-28 15:36:01 +0800

    car_driver、department第二次bugFix

--- rentManagement/rentManagement-business/rentManagement-business-deptAndEmployee/src/main/java/org/whut/rentManagement/business/deptAndEmployee/web/CarDriverServiceWeb.java
+++ rentManagement/rentManagement-business/rentManagement-business-deptAndEmployee/src/main/java/org/whut/rentManagement/business/deptAndEmployee/web/CarDriverServiceWeb.java
@@ -7,18 +7,22 @@ import org.whut.platform.fundamental.logger.PlatformLogger;
 import org.whut.platform.fundamental.util.json.JsonMapper;
 import org.whut.platform.fundamental.util.json.JsonResultUtils;
 import org.whut.rentManagement.business.deptAndEmployee.entity.CarDriver;
+import org.whut.rentManagement.business.deptAndEmployee.entity.SubCarDriver;
 
 import javax.ws.rs.FormParam;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
 @Component
 @Path("/car_Driver")
 public class CarDriverServiceWeb {
+
     private static PlatformLogger logger = PlatformLogger.getLogger(CarDriverServiceWeb.class);
     @Autowired
     private org.whut.rentManagement.business.deptAndEmployee.service.CarDriverService carDriverService;
@@ -53,33 +57,74 @@ public class CarDriverServiceWeb {
         else
             return JsonResultUtils.getCodeAndMesByString(JsonResultUtils.Code.ERROR.getCode(), "该司机已存在");
 
-     }
+    }
     @Produces( MediaType.APPLICATION_JSON + ";charset=UTF-8")
     @Path("/update")
     @POST
     public String update(@FormParam("jsonString") String jsonString)
     {
-        CarDriver car_driver = JsonMapper.buildNonDefaultMapper().fromJson(jsonString,CarDriver.class);
-        if ((car_driver.getName()==null||car_driver.getName().trim().equals(""))){
-            return JsonResultUtils.getCodeAndMesByString(JsonResultUtils.Code.ERROR.getCode(), "名字不能为空");
-        }else{
-            if ((car_driver.getCarNumber()==null||car_driver.getCarNumber().trim().equals(""))){
-                return JsonResultUtils.getCodeAndMesByString(JsonResultUtils.Code.ERROR.getCode(), "车牌号不能为空");
-            }
-        }
+        SubCarDriver subCarDriver =  JsonMapper.buildNonDefaultMapper().fromJson(jsonString,SubCarDriver.class);
         long appId = UserContext.currentUserAppId();
-        long id;
-        try{
-            id=carDriverService.getIdByCarNumber(car_driver.getCarNumber(), appId);
-        }catch (Exception e) {
-            id = 0;
+        if ((subCarDriver.getName()==null||subCarDriver.getName().equals(""))){
+            return JsonResultUtils.getCodeAndMesByString(JsonResultUtils.Code.ERROR.getCode(), "名字不能为空");
         }
-        if(id==0||id==car_driver.getId()) {
-            carDriverService.update(car_driver);
+        if ((subCarDriver.getCarNumber()==null||subCarDriver.getCarNumber().equals(""))){
+            return JsonResultUtils.getCodeAndMesByString(JsonResultUtils.Code.ERROR.getCode(), "车牌号不能为空");
+        }
+        if ((subCarDriver.getCreateTime()==null)){
+            CarDriver carDriverNoDate = new CarDriver();
+            carDriverNoDate.setId(subCarDriver.getId());
+            carDriverNoDate.setName(subCarDriver.getName());
+            carDriverNoDate.setCarNumber(subCarDriver.getCarNumber());
+            carDriverNoDate.setCarType(subCarDriver.getCarType());
+            carDriverNoDate.setAppId(appId);
+            long dpsNoDate;
+            try{
+                dpsNoDate=carDriverService.getIdByCarNumber(subCarDriver.getCarNumber(), appId);
+            }catch (Exception e) {
+                dpsNoDate = 0;
+            }
+            if(dpsNoDate!=0) {
+                if(dpsNoDate!=subCarDriver.getId()){
+                    return JsonResultUtils.getCodeAndMesByString(JsonResultUtils.Code.ERROR.getCode(), "修改的车牌已存在！");
+                }
+            }
+            carDriverService.update(carDriverNoDate);
             return JsonResultUtils.getCodeAndMesByStringAsDefault(JsonResultUtils.Code.SUCCESS);
         }
-        else
-            return JsonResultUtils.getCodeAndMesByString(JsonResultUtils.Code.ERROR.getCode(), "修改的部门已存在！");
+        CarDriver carDriver = new CarDriver();
+        Date date= null;
+        carDriver.setId(subCarDriver.getId());
+        carDriver.setName(subCarDriver.getName());
+        carDriver.setCarNumber(subCarDriver.getCarNumber());
+        carDriver.setCarType(subCarDriver.getCarType());
+        carDriver.setAppId(appId);
+        SimpleDateFormat DFT = new SimpleDateFormat("yyyy-MM-dd");
+        try{
+            date=DFT.parse(subCarDriver.getCreateTime());
+        }catch (Exception e){
+            JsonResultUtils.getCodeAndMesByString(JsonResultUtils.Code.ERROR.getCode(),"日期格式错误");
+        }
+        //调节时区，解决更新时间会显示前一天的问题
+        Calendar c = Calendar.getInstance();
+        c.setTime(date);
+        int day = c.get(Calendar.DATE);
+        c.set(Calendar.DATE, day + 1);
+        Date dateAfter = c.getTime();
+        carDriver.setCreateTime(dateAfter);
+        long dps;
+        try{
+            dps=carDriverService.getIdByCarNumber(subCarDriver.getCarNumber(), appId);
+        }catch (Exception e) {
+            dps = 0;
+        }
+        if(dps!=0) {
+            if(dps!=subCarDriver.getId()){
+                return JsonResultUtils.getCodeAndMesByString(JsonResultUtils.Code.ERROR.getCode(), "修改的司机已存在！");
+            }
+        }
+        carDriverService.update(carDriver);
+        return JsonResultUtils.getCodeAndMesByStringAsDefault(JsonResultUtils.Code.SUCCESS);
     }
     @Produces(MediaType.APPLICATION_JSON +";charset=UTF-8")
     @Path("/delete")

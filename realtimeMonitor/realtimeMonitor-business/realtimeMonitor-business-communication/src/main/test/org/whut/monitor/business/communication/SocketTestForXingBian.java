9eb9a7a9a0d4af45a49659e8522bc0130f7a69f3
xiaozhujun <xiaozhujun520@163.com>
2014-11-08 13:50:27 +0800

    完善波形的随机值

--- realtimeMonitor/realtimeMonitor-business/realtimeMonitor-business-communication/src/main/test/org/whut/monitor/business/communication/SocketTestForXingBian.java
+++ realtimeMonitor/realtimeMonitor-business/realtimeMonitor-business-communication/src/main/test/org/whut/monitor/business/communication/SocketTestForXingBian.java
@@ -28,45 +28,49 @@ public class SocketTestForXingBian {
 
             //向服务器端第二次发送字符串
             long count = 1000000;
-            StringBuffer data = new StringBuffer("");
-            StringBuffer data1 = new StringBuffer("");
+//            StringBuffer random[] = new StringBuffer("");
+//            StringBuffer random[]1 = new StringBuffer("");
             Date startTime = new Date();
             SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 
             for (long j=0;j<count;j++){
-                data.append(Math.round(Math.random()*500));
+//                random[].append(Math.round(Math.random()*600));
+                long random[] = new long[24];
+                for(int x=0;x<24;x++){
+                    random[x] = Math.round(Math.random()*600);
+                }
                 Date now = new Date();
-                String json = "{sensors:["+"{sensorNum:'2100000000010000',dataType:'Route',time:'"+format.format(now)+"',data:["+data+"]},"
-                        +"{sensorNum:'2100000000010001',dataType:'Route',time:'"+format.format(now)+"',data:["+data+"]},"
-                        +"{sensorNum:'2100000000010002',dataType:'Route',time:'"+format.format(now)+"',data:["+data+"]},"
-                        +"{sensorNum:'2100000000010003',dataType:'Route',time:'"+format.format(now)+"',data:["+data+"]},"
-                        +"{sensorNum:'2100000000010004',dataType:'Route',time:'"+format.format(now)+"',data:["+data+"]},"
-                        +"{sensorNum:'2100000000010005',dataType:'Route',time:'"+format.format(now)+"',data:["+data+"]},"
-                        +"{sensorNum:'2100000000010006',dataType:'Route',time:'"+format.format(now)+"',data:["+data+"]},"
-                        +"{sensorNum:'2100000000010007',dataType:'Route',time:'"+format.format(now)+"',data:["+data+"]},"
-                        +"{sensorNum:'2100000000010008',dataType:'Route',time:'"+format.format(now)+"',data:["+data+"]},"
-                        +"{sensorNum:'2100000000010009',dataType:'Route',time:'"+format.format(now)+"',data:["+data+"]},"
-                        +"{sensorNum:'2100000000010010',dataType:'Route',time:'"+format.format(now)+"',data:["+data+"]},"
-                        +"{sensorNum:'2100000000010011',dataType:'Route',time:'"+format.format(now)+"',data:["+data+"]},"
-                        +"{sensorNum:'2100000000010012',dataType:'Route',time:'"+format.format(now)+"',data:["+data+"]},"
-                        +"{sensorNum:'2100000000010013',dataType:'Route',time:'"+format.format(now)+"',data:["+data+"]},"
-                        +"{sensorNum:'2100000000010014',dataType:'Route',time:'"+format.format(now)+"',data:["+data+"]},"
-                        +"{sensorNum:'2100000000010015',dataType:'Route',time:'"+format.format(now)+"',data:["+data+"]},"
-                        +"{sensorNum:'2100000000010016',dataType:'Route',time:'"+format.format(now)+"',data:["+data+"]},"
-                        +"{sensorNum:'2100000000010017',dataType:'Route',time:'"+format.format(now)+"',data:["+data+"]},"
-                        +"{sensorNum:'2100000000010018',dataType:'Route',time:'"+format.format(now)+"',data:["+data+"]},"
-                        +"{sensorNum:'2100000000010019',dataType:'Route',time:'"+format.format(now)+"',data:["+data+"]},"
-                        +"{sensorNum:'2100000000010020',dataType:'Route',time:'"+format.format(now)+"',data:["+data+"]},"
-                        +"{sensorNum:'2100000000010021',dataType:'Route',time:'"+format.format(now)+"',data:["+data+"]},"
-                        +"{sensorNum:'2100000000010022',dataType:'Route',time:'"+format.format(now)+"',data:["+data+"]},"
-                        +"{sensorNum:'2100000000010023',dataType:'Route',time:'"+format.format(now)+"',data:["+data+"]}]}";
+                String json = "{sensors:["+"{sensorNum:'2100000000010000',dataType:'Route',time:'"+format.format(now)+"',data:["+random[0]+"]},"
+                        +"{sensorNum:'2100000000010001',dataType:'Route',time:'"+format.format(now)+"',data:["+random[1]+"]},"
+                        +"{sensorNum:'2100000000010002',dataType:'Route',time:'"+format.format(now)+"',data:["+random[2]+"]},"
+                        +"{sensorNum:'2100000000010003',dataType:'Route',time:'"+format.format(now)+"',data:["+random[3]+"]},"
+                        +"{sensorNum:'2100000000010004',dataType:'Route',time:'"+format.format(now)+"',data:["+random[4]+"]},"
+                        +"{sensorNum:'2100000000010005',dataType:'Route',time:'"+format.format(now)+"',data:["+random[5]+"]},"
+                        +"{sensorNum:'2100000000010006',dataType:'Route',time:'"+format.format(now)+"',data:["+random[6]+"]},"
+                        +"{sensorNum:'2100000000010007',dataType:'Route',time:'"+format.format(now)+"',data:["+random[7]+"]},"
+                        +"{sensorNum:'2100000000010008',dataType:'Route',time:'"+format.format(now)+"',data:["+random[8]+"]},"
+                        +"{sensorNum:'2100000000010009',dataType:'Route',time:'"+format.format(now)+"',data:["+random[9]+"]},"
+                        +"{sensorNum:'2100000000010010',dataType:'Route',time:'"+format.format(now)+"',data:["+random[10]+"]},"
+                        +"{sensorNum:'2100000000010011',dataType:'Route',time:'"+format.format(now)+"',data:["+random[11]+"]},"
+                        +"{sensorNum:'2100000000010012',dataType:'Route',time:'"+format.format(now)+"',data:["+random[12]+"]},"
+                        +"{sensorNum:'2100000000010013',dataType:'Route',time:'"+format.format(now)+"',data:["+random[13]+"]},"
+                        +"{sensorNum:'2100000000010014',dataType:'Route',time:'"+format.format(now)+"',data:["+random[14]+"]},"
+                        +"{sensorNum:'2100000000010015',dataType:'Route',time:'"+format.format(now)+"',data:["+random[15]+"]},"
+                        +"{sensorNum:'2100000000010016',dataType:'Route',time:'"+format.format(now)+"',data:["+random[16]+"]},"
+                        +"{sensorNum:'2100000000010017',dataType:'Route',time:'"+format.format(now)+"',data:["+random[17]+"]},"
+                        +"{sensorNum:'2100000000010018',dataType:'Route',time:'"+format.format(now)+"',data:["+random[18]+"]},"
+                        +"{sensorNum:'2100000000010019',dataType:'Route',time:'"+format.format(now)+"',data:["+random[19]+"]},"
+                        +"{sensorNum:'2100000000010020',dataType:'Route',time:'"+format.format(now)+"',data:["+random[20]+"]},"
+                        +"{sensorNum:'2100000000010021',dataType:'Route',time:'"+format.format(now)+"',data:["+random[21]+"]},"
+                        +"{sensorNum:'2100000000010022',dataType:'Route',time:'"+format.format(now)+"',data:["+random[22]+"]},"
+                        +"{sensorNum:'2100000000010023',dataType:'Route',time:'"+format.format(now)+"',data:["+random[23]+"]}]}";
                 writer.write(json);
                 writer.flush();//写完后要记得flush
                 System.out.println(json);
-                data.delete(0,data.length());
-                data1.delete(0,data1.length());
+//                random[].delete(0,random[].length());
+//                random[]1.delete(0,random[]1.length());
                 try {
-                    Thread.sleep(2000);
+                    Thread.sleep(5000);
                 } catch (InterruptedException e) {
                     e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
                 }

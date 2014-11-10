c12b52a9d05ddb4420eed0a448960e3656c6e99f
xiaozhujun <xiao520@163.com>
2014-11-10 21:36:44 +0800

    添加命令执行组件（websocket的订阅和取消订阅）

--- fundamental/fundamental-websocket/src/main/java/org/whut/platform/fundamental/websocket/handler/WebsocketEndPoint.java
+++ fundamental/fundamental-websocket/src/main/java/org/whut/platform/fundamental/websocket/handler/WebsocketEndPoint.java
@@ -1,20 +1,16 @@
 package org.whut.platform.fundamental.websocket.handler;
 
 import org.codehaus.jettison.json.JSONArray;
-import org.codehaus.jettison.json.JSONException;
 import org.codehaus.jettison.json.JSONObject;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 import org.whut.platform.business.user.security.MyUserDetail;
-import org.whut.platform.fundamental.communication.api.MessageDispatcher;
 import org.springframework.security.core.context.SecurityContext;
+import org.whut.platform.fundamental.command.CommandProcessing;
 import org.whut.platform.fundamental.logger.PlatformLogger;
 
-import java.io.IOException;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -28,11 +24,8 @@ import java.util.Map;
  */
 public class WebsocketEndPoint extends TextWebSocketHandler {
     private static final PlatformLogger logger = PlatformLogger.getLogger(WebsocketEndPoint.class);
-    private static List<WebSocketSession> wssList;   // ReceivedMessage对应的 WebSocketSession列表
     private static Map<WebSocketSession,String> map=new HashMap<WebSocketSession, String>(); // 一个session对应一个message 维护 session和message的关系
-    private static Map<String,List<WebSocketSession>> wsImpMap=new HashMap<String, List<WebSocketSession>>(); //一个message对应多个session，供通过websocket向前台发送消息使用
     private static Map<WebSocketSession,Long> sessionAndAppIdMap= new HashMap<WebSocketSession,Long>();  //维护session和appId的关系，
-    private static Map<String,WebSocketSession> wsuImpMap=new HashMap<String,WebSocketSession>();
 
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
@@ -54,65 +47,22 @@ public class WebsocketEndPoint extends TextWebSocketHandler {
         //得到前台发来的命令：取消订阅、订阅
         String command=dataJson.getString("c");
         if (command.equals("cancelSubscribe")){    //取消订阅
-                logger.info("进行取消订阅！");
-                logger.info("wsImpMap1为"+wsImpMap);
-                cancelSubscribe(ReceivedMessage,appId,userId);
-                logger.info("wsImpMap1为"+wsImpMap);
-        } else{
-            if (command.equals("Subscribe")){    //订阅
-                 for(int i=0;i<sNum.length();i++){
-                    wssList=wsImpMap.get(appId+":"+sNum.get(i).toString());
-                   if(wssList==null){
-                       wssList=new ArrayList<WebSocketSession>();
-                   }
-                   wssList.add(session);
-                   wsImpMap.put(appId+":"+sNum.get(i).toString(),wssList);
-                   wsuImpMap.put(appId+":"+userId+":"+sNum.get(i).toString(),session);
-                }
-        }}
+            CommandProcessing.cancelSubscribe(ReceivedMessage, appId, userId);
+        } else if (command.equals("Subscribe")){    //订阅
+            CommandProcessing.subscribe(appId, userId, sNum, session);
+        }
         super.handleTextMessage(session, message);
     }
 
     @Override
     public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
-        cancelSubscribe(session);
+        CommandProcessing.cancelSubscribe(map,session,sessionAndAppIdMap);
+        map.remove(session);
+        sessionAndAppIdMap.remove(session);
         logger.info("连接关闭！");
         super.afterConnectionClosed(session, status);
     }
-
-    public void cancelSubscribe(WebSocketSession session) throws JSONException {
-        String msg= map.get(session);     //获得接收到的sNum 数组
-        Long appId=sessionAndAppIdMap.get(session);
-        JSONObject dataJson=new JSONObject(msg);
-        JSONArray sNum= dataJson.getJSONArray("sensors");
-        for(int i=0;i<sNum.length();i++){
-            logger.info("wsImpMap为"+wsImpMap);
-            List<WebSocketSession> webSocketSessionList=wsImpMap.get(appId+":"+sNum.get(i).toString());
-            webSocketSessionList.remove(session);
-            if(webSocketSessionList.isEmpty()){
-                wsImpMap.remove(appId+":"+sNum.get(i).toString());
-            }
-            logger.info("wsImpMap为"+wsImpMap);
-        }
-        map.remove(session);
-        sessionAndAppIdMap.remove(session);
-    }
-
-    public void cancelSubscribe(String msg,Long appId,Long userId) throws JSONException {
-        JSONObject dataJson=new JSONObject(msg);
-        JSONArray sNum= dataJson.getJSONArray("sensors");
-        for(int i=0;i<sNum.length();i++){
-           WebSocketSession webSocketSession=wsuImpMap.get(appId+":"+userId+":"+sNum.get(i).toString());
-           List<WebSocketSession> webSocketSessionList=wsImpMap.get(appId+":"+sNum.get(i).toString());
-           webSocketSessionList.remove(webSocketSession);
-            if(webSocketSessionList.isEmpty()){
-                wsImpMap.remove(appId+":"+sNum.get(i).toString());
-            }
-           wsuImpMap.remove(appId+":"+userId+":"+sNum.get(i).toString());
-        }
-    }
-
     public static Map<String,List<WebSocketSession>> getWsImpMap(){
-        return wsImpMap;
+        return CommandProcessing.wsImpMap;
     }
 }

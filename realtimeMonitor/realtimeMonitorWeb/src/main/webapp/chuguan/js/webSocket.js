16c6d285b9972d4243e3d9517c6d38824d2f3933
xiaozhujun <xiao520@163.com>
2014-11-14 13:19:59 +0800

    websocket的js重构，完成储油罐管理系统页面中的websocket部分

---
+++ realtimeMonitor/realtimeMonitorWeb/src/main/webapp/chuguan/js/webSocket.js
@@ -0,0 +1,18 @@
+
+ $.extend({
+    initWebSocket:function initWebSocket(s,method)
+    {
+       var url= $.URL.websocket.register;
+       ws = new WebSocket(url);
+       ws.onopen = function(){
+           setInterval(function(){$.post($.URL.user.keepAlive,null,null)},1000*60);
+           console.log("open"); ws.send(s);};
+       ws.onmessage=function (event){method(event.data);};
+       ws.onclose =function onclose(evt){console.log("WebSocketClosed!");};
+       ws.onerror =function onerror(evt){console.log("WebSocketError!");};
+    }}
+ );
+
+
+
+

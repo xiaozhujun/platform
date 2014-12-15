dee0a1ee232cc4d63c7014ec15df219096505eb2
yangyang1992 <1570444319@qq.com>
2014-12-15 20:35:05 +0800

    将消息发送至websocket队列并添加设备模块，设备模块有待完善

---
+++ trackSystem/trackSystem-business/trackSystem-business-device/src/main/java/org/whut/trackSystem/business/device/mapper/DeviceMapper.java
@@ -0,0 +1,18 @@
+package org.whut.trackSystem.business.device.mapper;
+
+import org.whut.platform.fundamental.orm.mapper.AbstractMapper;
+import org.whut.trackSystem.business.device.entity.Device;
+
+import java.util.List;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: yangyang
+ * Date: 14-12-15
+ * Time: 下午4:14
+ * To change this template use File | Settings | File Templates.
+ */
+public interface DeviceMapper extends AbstractMapper<Device> {
+    public List<Device> getListByAppId(long appId);
+    public Long getAppIdByDeviceNum(String number);
+}
---
+++ trackSystem/trackSystem-business/trackSystem-business-device/src/main/java/org/whut/trackSystem/business/device/mapper/DeviceTypeMapper.java
@@ -0,0 +1,11 @@
+package org.whut.trackSystem.business.device.mapper;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: yangyang
+ * Date: 14-12-15
+ * Time: 下午4:19
+ * To change this template use File | Settings | File Templates.
+ */
+public interface DeviceTypeMapper {
+}
